f392aca510a0de20485fd919dd761b295fa43de8
William Grant <hello@wpg.io>
2024-05-23 14:28:21 -0400

    unpacker hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,49 +1,68 @@
 import os
 import shutil
 import subprocess
+import sys
 
-include_pytest = '{{ cookiecutter.include_pytest }}'
-include_mkdocs = '{{ cookiecutter.include_mkdocs }}'
-include_cli = '{{ cookiecutter.include_cli }}'
+include_pytest = "{{ cookiecutter.include_pytest }}"
+include_mkdocs = "{{ cookiecutter.include_mkdocs }}"
+include_cli = "{{ cookiecutter.include_cli }}"
 
-docs_path = 'docs'
-tests_path = 'tests'
-cli_path = os.path.join('src', '{{ cookiecutter.package_name }}', 'cli.py')
+docs_path = "docs"
+tests_path = "tests"
+cli_path = os.path.join("src", "{{ cookiecutter.package_name }}", "cli.py")
+
 
 # Function to remove directories
 def remove_directory(path):
     if os.path.exists(path):
         shutil.rmtree(path)
 
+
 # Function to remove files
 def remove_file(path):
     if os.path.exists(path):
         os.remove(path)
 
+
 if __name__ == "__main__":
     # Remove directories or files based on user input
-    if include_pytest == 'n':
+    if include_pytest == "n":
         remove_directory(tests_path)
 
-    if include_mkdocs == 'n':
+    if include_mkdocs == "n":
         remove_directory(docs_path)
-        remove_file('mkdocs.yml')
+        remove_file("mkdocs.yml")
 
-    if include_cli == 'n':
+    if include_cli == "n":
         remove_file(cli_path)
 
     # install hooks
-    subprocess.run(['pip', 'install', 'pre-commit'])
-    subprocess.run(['pre-commit', 'install'])
+    if os.path.exists(".git"):
+        subprocess.run(["pip", "install", "pre-commit"])
+        subprocess.run(["pre-commit", "install"])
 
-    # # move all the 
-    # # Get the current directory path
-    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    # unpack the template into the parent dir
+    current_dir = os.getcwd()
+    parent_dir = os.path.dirname(current_dir)
+    will_overwrite = False
+    items = []
+    for item in os.listdir(current_dir):
+        if item != "__pycache__" and os.path.exists(os.path.join(parent_dir, item)):
+            items.append(item)
+            will_overwrite = True
 
-    # # Get the parent directory path
-    # parent_dir = os.path.dirname(current_dir)
+    if will_overwrite:
+        print("Moving the generated files to the parent directory will overwrite existing files:")
+        for item in items:
+            print(f"\t{item}")
+        confirm = input("Do you want to proceed? (y/n): ")
+        if confirm.lower() != "y":
+            print("Aborted moving files to avoid overwriting.")
+            sys.exit(1)
 
-    # # Move the generated files to the parent directory
-    # for item in os.listdir(current_dir):
-    #     if item != '__pycache__':
-    #         shutil.move(os.path.join(current_dir, item), parent_dir)
+    # Move the generated files to the parent directory
+    for item in os.listdir(current_dir):
+        if item != "__pycache__":
+            shutil.move(os.path.join(current_dir, item), os.path.join(parent_dir, item))
+
+    os.rmdir(current_dir)
